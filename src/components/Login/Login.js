03a8ba223604c93226f83ad9d88ea2397dbd4407
sadik182 <sadikurrahman728@gmail.com>
2022-10-08 03:00:31 +0600

    google login implemented

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,43 +3,49 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useRef } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from "react-firebase-hooks/auth";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import auth from "../firebase/firebase.init";
-import './Login.css'
+import "./Login.css";
 
 const Login = () => {
-  const emailRef = useRef('');
-  const passwordRef = useRef('');
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
   const navigate = useNavigate();
-  const location = useLocation()
-  const [
-    signInWithEmailAndPassword,
-    user,
-    loading,
-    error,
-  ] = useSignInWithEmailAndPassword(auth);
+  const location = useLocation();
+  const [signInWithEmailAndPassword, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+  const [signInWithGoogle, user] = useSignInWithGoogle(auth);
 
-  const from = location.state?.from?.pathname || '/';
+  const from = location.state?.from?.pathname || "/";
+  const GoogleLogin = (e) => {
+    e.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    signInWithGoogle(email, password);
+  };
 
-  const handleLogin = e => {
+  const handleLogin = (e) => {
     e.preventDefault();
     const email = emailRef.current.value;
     const password = passwordRef.current.value;
     console.log(email, password);
-    signInWithEmailAndPassword(email, password)
-
-  }
-  if(user) {
-    navigate(from, {replace: true});
+    signInWithEmailAndPassword(email, password);
+  };
+  if (user) {
+    navigate(from, { replace: true });
   }
   return (
-    <div>
-        <h2 className='my-3'>If You Have an Account Please Login</h2>
-      <Form onSubmit={handleLogin} className="login_form">
+    <div className="login_form">
+      <h2 className="my-3">If You Have an Account Login</h2>
+      <Form onSubmit={handleLogin}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label className='form-label'>Email address</Form.Label>
-          <Form.Control ref={emailRef} type="email" placeholder="Enter Your Email" />
+          <Form.Label className="form-label">Email address</Form.Label>
+          <Form.Control
+            ref={emailRef}
+            type="email"
+            placeholder="Enter Your Email"
+          />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -47,16 +53,24 @@ const Login = () => {
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control ref={passwordRef} type="password" placeholder="Enter Password" />
+          <Form.Control
+            ref={passwordRef}
+            type="password"
+            placeholder="Enter Password"
+          />
         </Form.Group>
-        <Button variant="primary" type="submit" size='lg'>
+        <Button variant="primary" type="submit" size="lg">
           Login <FontAwesomeIcon icon={faSignIn} />
         </Button>
-        <p>Don't have an account? <Link to='/register'>Create an Account</Link>  </p>
+        <p>
+          Don't have an account? <Link to="/register">Create an Account</Link>{" "}
+        </p>
         <div>-----------OR-------------</div>
-        <Button variant="outline-primary" className="me-3">Google Login</Button> 
-        <Button variant='outline-primary'>Facebook Login </Button>
       </Form>
+      <Button variant="outline-primary" className="me-3" onClick={GoogleLogin}>
+        Google Login
+      </Button>
+      <Button variant="outline-primary">Facebook Login </Button>
     </div>
   );
 };
